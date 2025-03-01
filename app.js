842ffc879017157344efb5298e8ec0ea47faed4d
RohitNippane <rohitnippane2003@gmail.com>
2025-03-01 16:00:08 +0530

    task-4

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const morgan = require('morgan');
+const {Product} = require('./model/Product');
 
 
 //connecting to database
-mongoose.connect('mongodb://127.0.0.1:27017/shopify_ecom')
+mongoose.connect('mongodb://127.0.0.1:27017/Shopify_ecom')
 .then(()=>{
     console.log('Connected to database');
 }).catch((err)=>{
@@ -101,8 +102,43 @@ app.post('/login',async(req,res)=>{
     }
 })
 
+// task-3 -> create a route to see all product
+app.get('/products',async(req,res)=>{
+    try{
+        const products = await Product.find({});
+        return res.status(200).json({message:"Find all products",products:products});
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:'Internal server error'})
+    }
+})
 
 
+
+
+//task-4 -> create a route to add a product
+app.post('/add-product',async(req,res)=>{
+    try{
+        const{name,price,image,brand,stock,description}=req.body;
+        const {token}=req.headers;
+        const decodedToken=jwt.verify(token,'supersecret');
+        const user=await User.findOne({email:decodedToken.email});
+
+        await Product.create({
+            name,
+            price,
+            image,
+            brand,
+            stock,
+            description,
+            user:user._id
+        })
+        return res.status(201).json({message:'Product added successfully'});
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:'Internal server error'})
+    }
+})
 const PORT = 8080;
 app.listen(PORT,()=>{
     console.log(`Server is connected to port ${PORT}`);
